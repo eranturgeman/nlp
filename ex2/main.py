1bee8b5f3ed5bb475dbc83579b3a2d4e47bd4a4d
Dor D <dor.dahuki@gmail.com>
2022-11-19 12:57:23 +0200

    Update main.py

--- ex2/main.py
+++ ex2/main.py
@@ -1,27 +1,28 @@
 import nltk
 from nltk.corpus import brown
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from collections import defaultdict
 
 TEST_SET_PROPORTION = 0.1
 PROB_FOR_UNKNOWN = -float("inf")
 UNKNOWN_TAG = "NN"
 
-class PosModel:
-    def __init__(self, sentences):
-        self.sentences = sentences
+class BasicModel:
+    def __init__(self, train_set, test_set):
+        self.train_set = train_set
+        self.test_set = test_set
         self.word_tags = defaultdict(self.default)
         self.max_probability = dict()
 
     def fit(self):
-        for sentence in self.sentences:
+        for sentence in self.train_set:
             for word, tag in sentence:
                 self.word_tags[word][tag] += 1
 
         for word in self.word_tags.keys():
-            self.find_best_tag_probability(word)
+            self.predict_best_tag_probability(word)
 
-    def find_best_tag_probability(self, word):
+    def predict_best_tag_probability(self, word):
         if word not in self.max_probability:
             self.max_probability[word] = (PROB_FOR_UNKNOWN, UNKNOWN_TAG) # todo check if leaving -inf in unknown probabilities
             return self.max_probability
@@ -41,17 +42,29 @@ class PosModel:
 
         return self.max_probability[word]
 
+    def compute_error_rate(self):
+        total_words = 0
+        correct_tag_words = 0
+        for sentence in self.test_set:
+            for word, tag in sentence:
+                total_words += 1
+                if self.predict_best_tag_probability(word)[1] == tag:
+                    correct_tag_words += 1
 
+        accuracy = correct_tag_words / total_words
+        return 1 - accuracy
 
     @staticmethod
     def default():
         return defaultdict(int)
 
+class BigramHMM:
+    def __init__(self):
 
 if __name__ == '__main__':
     nltk.download('brown')
     sentences = brown.tagged_sents(categories='news')
-    # train_set, test_set = train_test_split(sentences, test_size=TEST_SET_PROPORTION)
-    model = PosModel(sentences)
+    train_set, test_set = train_test_split(sentences, test_size=TEST_SET_PROPORTION)
+    model = BasicModel(train_set, test_set)
     model.fit()
 
