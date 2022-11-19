47735a6ab84d78b5876f449da5d9a6f8240b461a
Dor D <dor.dahuki@gmail.com>
2022-11-19 14:15:12 +0200

    Update main.py

--- ex2/main.py
+++ ex2/main.py
@@ -7,6 +7,9 @@ TEST_SET_PROPORTION = 0.1
 PROB_FOR_UNKNOWN = -float("inf")
 UNKNOWN_TAG = "NN"
 
+
+
+
 class BasicModel:
     def __init__(self, train_set, test_set):
         self.train_set = train_set
@@ -60,11 +63,66 @@ class BasicModel:
 
 class BigramHMM:
     def __init__(self):
+        self.train_set = train_set
+        self.test_set = test_set
+        # self.tags_words_count = defaultdict(self.default)
+        self.categories_count = dict()
+        self.emission = defaultdict(self.default)
+
+
+    def fit(self):
+        self.fit_emission()
+        self.fit_transition()
+
+    def fit_emission(self):
+        tags_words_count = defaultdict(self.default)
+
+        for sentences in self.train_set:
+            for word, tag in sentences:
+                tags_words_count[tag][word] += 1
+
+        for tag, words in tags_words_count.items():
+            total_tag_count = sum(count for _, count in words)
+            for word, count in words.items():
+                self.emission[word][tag] = tags_words_count[tag][word] / total_tag_count
+
+    def fit_transition(self):
+        for sentence in self.train_set:
+
+    @staticmethod
+    def default():
+        return defaultdict(int)
+
+
+# general functions
+def filter_tag(tag):
+    #todo check if * can appear as prefix. if so modify this func to handle it
+    if '*' in tag:
+        idx = tag.find("*")
+        tag = tag[:idx]
+
+    if '-' in tag:
+        idx = tag.find("-")
+        tag = tag[:idx]
+
+    if '+' in tag:
+        idx = tag.find("+")
+        tag = tag[:idx]
+
+    return tag
+
+def clean_dataset(dataset):
+    clean = []
+    for sentence in dataset:
+        clean_sentence = [(word, filter_tag(tag)) for word, tag in sentence]
+        clean.append(clean_sentence)
+
+    return clean
 
 if __name__ == '__main__':
     nltk.download('brown')
-    sentences = brown.tagged_sents(categories='news')
-    train_set, test_set = train_test_split(sentences, test_size=TEST_SET_PROPORTION)
+    dataset = brown.tagged_sents(categories='news')
+    dataset = clean_dataset(dataset)
+    train_set, test_set = train_test_split(dataset, test_size=TEST_SET_PROPORTION)
     model = BasicModel(train_set, test_set)
     model.fit()
-
