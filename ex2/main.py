721690ae8d6eba60568bd36fc0dfa5f0b3142908
Dor D <dor.dahuki@gmail.com>
2022-11-25 17:31:03 +0200

    clean the code

--- ex2/main.py
+++ ex2/main.py
@@ -9,7 +9,6 @@ PROB_FOR_UNKNOWN = 1
 UNKNOWN_TAG = "NN"
 STARTING_TAG = "*"
 END_TAG = "STOP"
-VITERBI_UNKNOWN = "VUK"
 
 def default():
     return defaultdict(int)
@@ -85,6 +84,7 @@ class BigramHMM:
         self.tags_words_count = defaultdict(default)
         self.all_words = set()
         self.add_one_smoothing = add_one_smoothing
+        self.number_of_words = 0
 
     def fit(self):
         self.fit_emission()
@@ -96,23 +96,33 @@ class BigramHMM:
                 self.tags_words_count[tag][word] += 1
                 self.all_words.add(word)
 
+        self.number_of_words = len(self.all_words)
+
         for tag, words in self.tags_words_count.items():
+            total_tag_count = sum(self.tags_words_count[tag].values())
+
             for word in words:
                 if self.add_one_smoothing:
-                    self.emission[str(tag)][str(word)] = self.calc_smoothed_emission(word, tag)
+                    self.emission[tag][word] = (self.tags_words_count[tag][word] + 1) / (total_tag_count + self.number_of_words)
                 else:
-                    self.emission[str(tag)][str(word)] = self.calc_emission(word, tag)
+                    self.emission[tag][word] = self.tags_words_count[tag][word] / total_tag_count
 
-    def calc_emission(self, word, tag):
-        # if word not in self.tags_words_count[tag]:
-        #     return 0 # # if tag == UNKNOWN_TAG else 0 #todo check what should be returned for unseen word
+    def get_emission(self, word, category):
+        if word not in self.all_words:
+            if self.add_one_smoothing:
+                # unknown word with smoothing
+                total_tag_count = sum(self.tags_words_count[category].values())
+                e = 1 / (total_tag_count + self.number_of_words)
+            elif category == 'NN':
+                # unknown word without smoothing with NN tag todo check if 1 should be returned
+                e = 1
+            else:
+                # unknown word without smoothing without NN tag
+                e = 0
+        else:
+            e = self.emission[category][word]
 
-        total_tag_count = sum(count for count in self.tags_words_count[tag].values())
-        return self.tags_words_count[tag][word] / total_tag_count
-
-    def calc_smoothed_emission(self, word, tag):
-        total_tag_count = sum(count for count in self.tags_words_count[tag].values())
-        return (self.tags_words_count[tag][word] + 1) / (total_tag_count + len(self.all_words))
+        return e
 
     def fit_transition(self):
         for sentence in self.train_set:
@@ -124,14 +134,9 @@ class BigramHMM:
             self.categories_count[prev] += 1
             self.categories_pairs_count[prev][END_TAG] += 1
 
-        for first_tag, second_tags in self.categories_pairs_count.items():
-            for second_tag in second_tags.keys():
+        for first_tag, second_tags_dict in self.categories_pairs_count.items():
+            for second_tag in second_tags_dict.keys():
                 self.transition[first_tag][second_tag] = self.categories_pairs_count[first_tag][second_tag] / self.categories_count[first_tag]
-
-        import json
-
-        # print(json.dumps(self.transition, indent=2))
-        pass
 
     def viterbi(self, sentence):
         categories = list(self.categories_count.keys())
@@ -139,14 +144,12 @@ class BigramHMM:
         num_categories = len(categories)
         n = len(sentence)
 
-        backpointers = np.zeros((n, num_categories))
         pi = np.zeros((n, num_categories))
+        backpointers = np.zeros((n, num_categories))
+
         for k, (word, _) in enumerate(sentence):
             for cur_category_idx, cur_category in enumerate(categories):
-                if word not in self.all_words and cur_category == 'NN':
-                    e = 1
-                else:
-                    e = self.emission[cur_category][word]
+                e = self.get_emission(word, cur_category)
 
                 if k == 0:
                     pi[k, cur_category_idx] = 1 * self.transition[STARTING_TAG][cur_category] * e
@@ -162,35 +165,26 @@ class BigramHMM:
                     pi[k, cur_category_idx] = max_calc
                     backpointers[k, cur_category_idx] = max_category_idx
 
+        return self.get_category_path(backpointers, categories, n, pi)
+
+    def get_category_path(self, backpointers, categories, n, pi):
         # Find the last category
         max_cat_idx = None
         max_cat_prob = -float('inf')
-
-        for cat_idx, cat_val in enumerate(pi[n-1]):
+        for cat_idx, cat_val in enumerate(pi[n - 1]):
             p = cat_val * self.transition[categories[cat_idx]][END_TAG]
             if p > max_cat_prob:
                 max_cat_idx = cat_idx
                 max_cat_prob = p
-
         predicted_categories = [categories[max_cat_idx]]
         backpointers = backpointers.astype(int)
-        for idx in range(n-1, 0, -1):
+
+        for idx in range(n - 1, 0, -1):
             prev_cat_idx = backpointers[idx, max_cat_idx]
             predicted_categories.append(categories[prev_cat_idx])
             max_cat_idx = prev_cat_idx
 
         return predicted_categories[::-1]
-
-        #for the k-th word in sentence:
-        #   for cur_cat in categories:
-        #       if k == 1:
-        #           ==== pi(0, *) = 1 ====
-        #           x_k = pi(k - 1, *) * q(cur_cat | *) * e(word | cur_cat)
-        #           pi(k, cur_cat) = x_k
-        #       else:
-        #           for prev_cat in categories:
-        #                x_k = pi(k - 1, prev_cat) * q(cur_cat | prev_cat) * e(word | cur_cat)
-        #           pi(k, cur_cat) = argmax{ x_k from prev_cat }
 
     def compute_error_rate(self):
         total_words = 0
@@ -204,7 +198,6 @@ class BigramHMM:
             predicted_tags = self.viterbi(sentence)
 
             for (word, true_tag), predicted_tag in zip(sentence, predicted_tags):
-                # print(true_tag, predicted_tag)
                 total_words += 1
 
                 if word in self.all_words:
@@ -224,7 +217,6 @@ class BigramHMM:
         known_words_accuracy = known_words_correct_tag / known_words
         unknown_words_accuracy = unknown_words_correct_tag / unknown_words
         return 1 - total_words_accuracy, 1 - known_words_accuracy, 1 - unknown_words_accuracy  # total error, know error, unknown error
-
 
 
 # general functions
